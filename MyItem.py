ae7855dc5312d1709e0b59c7d1b6b17abd665a19
Xuemin Guan <xueminguan@Xuemins-MacBook-Pro.local>
2025-10-21 14:17:40 +0100

    XG: first cut of refactoring. Moving quality update logic into MyItem.py which paves the way for using polymorphism to capture the update quality logic

---
+++ MyItem.py
@@ -0,0 +1,35 @@
+from gilded_rose import Item
+
+
+class MyItem(Item):
+    def __init__(self, name, sell_in, quality):
+        super().__init__(name, sell_in, quality)
+
+    def update_quality(self):
+        if self.name != "Aged Brie" and self.name != "Backstage passes to a TAFKAL80ETC concert":
+            if self.quality > 0:
+                if self.name != "Sulfuras, Hand of Ragnaros":
+                    self.quality = self.quality - 1
+        else:
+            if self.quality < 50:
+                self.quality = self.quality + 1
+                if self.name == "Backstage passes to a TAFKAL80ETC concert":
+                    if self.sell_in < 11:
+                        if self.quality < 50:
+                            self.quality = self.quality + 1
+                    if self.sell_in < 6:
+                        if self.quality < 50:
+                            self.quality = self.quality + 1
+        if self.name != "Sulfuras, Hand of Ragnaros":
+            self.sell_in = self.sell_in - 1
+        if self.sell_in < 0:
+            if self.name != "Aged Brie":
+                if self.name != "Backstage passes to a TAFKAL80ETC concert":
+                    if self.quality > 0:
+                        if self.name != "Sulfuras, Hand of Ragnaros":
+                            self.quality = self.quality - 1
+                else:
+                    self.quality = self.quality - self.quality
+            else:
+                if self.quality < 50:
+                    self.quality = self.quality + 1
--- texttest_fixture.py
+++ texttest_fixture.py
@@ -1,21 +1,22 @@
 # -*- coding: utf-8 -*-
 from __future__ import print_function
 
+from MyItem import MyItem
 from gilded_rose import *
 
 
 def main():
     print("OMGHAI!")
     items = [
-        Item(name="+5 Dexterity Vest", sell_in=10, quality=20),
-        Item(name="Aged Brie", sell_in=2, quality=0),
-        Item(name="Elixir of the Mongoose", sell_in=5, quality=7),
-        Item(name="Sulfuras, Hand of Ragnaros", sell_in=0, quality=80),
-        Item(name="Sulfuras, Hand of Ragnaros", sell_in=-1, quality=80),
-        Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=15, quality=20),
-        Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=10, quality=49),
-        Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=5, quality=49),
-        Item(name="Conjured Mana Cake", sell_in=3, quality=6),  # <-- :O
+        MyItem(name="+5 Dexterity Vest", sell_in=10, quality=20),
+        MyItem(name="Aged Brie", sell_in=2, quality=0),
+        MyItem(name="Elixir of the Mongoose", sell_in=5, quality=7),
+        MyItem(name="Sulfuras, Hand of Ragnaros", sell_in=0, quality=80),
+        MyItem(name="Sulfuras, Hand of Ragnaros", sell_in=-1, quality=80),
+        MyItem(name="Backstage passes to a TAFKAL80ETC concert", sell_in=15, quality=20),
+        MyItem(name="Backstage passes to a TAFKAL80ETC concert", sell_in=10, quality=49),
+        MyItem(name="Backstage passes to a TAFKAL80ETC concert", sell_in=5, quality=49),
+        MyItem(name="Conjured Mana Cake", sell_in=3, quality=6),  # <-- :O
     ]
     days = 2
     import sys
