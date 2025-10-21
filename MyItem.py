71ab1139ed8feeedfe7c0f5452ae47e19cbc7473
Xuemin Guan <xueminguan@Xuemins-MacBook-Pro.local>
2025-10-21 15:13:46 +0100

    XG: implementing the update logic for Aged Brie item in its own class.

--- MyItem.py
+++ MyItem.py
@@ -3,7 +3,7 @@ from gilded_rose import Item
 
 class MyItem(Item):
     def update_quality(self):
-        if self.name != "Aged Brie" and self.name != "Backstage passes to a TAFKAL80ETC concert":
+        if self.name != "Backstage passes to a TAFKAL80ETC concert":
             if self.quality > 0:
                 if self.name != "Sulfuras, Hand of Ragnaros":
                     self.quality = self.quality - 1
@@ -20,31 +20,39 @@ class MyItem(Item):
         if self.name != "Sulfuras, Hand of Ragnaros":
             self.sell_in = self.sell_in - 1
         if self.sell_in < 0:
-            if self.name != "Aged Brie":
-                if self.name != "Backstage passes to a TAFKAL80ETC concert":
-                    if self.quality > 0:
-                        if self.name != "Sulfuras, Hand of Ragnaros":
-                            self.quality = self.quality - 1
-                else:
-                    self.quality = self.quality - self.quality
+            if self.name != "Backstage passes to a TAFKAL80ETC concert":
+                if self.quality > 0:
+                    if self.name != "Sulfuras, Hand of Ragnaros":
+                        self.quality = self.quality - 1
             else:
-                if self.quality < 50:
-                    self.quality = self.quality + 1
+                self.quality = self.quality - self.quality
 
     @staticmethod
     def make_item(name: str, sell_in: int, quality: int):
         if name.startswith("Conjured"):
             return ConjuredItem(name, sell_in, quality)
+        elif name == "Aged Brie":
+            return AgedBrieItem(name, sell_in, quality)
         else:
             return MyItem(name, sell_in, quality)
 
+
 # Conjured items degrade in quality twice as fast
 class ConjuredItem(MyItem):
-   def update_quality(self):
-       self.sell_in = self.sell_in - 1
+    def update_quality(self):
+        self.sell_in = self.sell_in - 1
 
-       if self.sell_in < 0 and self.quality >= 2:
-           self.quality = self.quality - 2
-       elif self.quality >= 1:
-           self.quality = self.quality - 1
+        if self.sell_in < 0 and self.quality >= 2:
+            self.quality = self.quality - 2
+        elif self.quality >= 1:
+            self.quality = self.quality - 1
 
+
+class AgedBrieItem(MyItem):
+    def update_quality(self):
+        self.sell_in = self.sell_in - 1
+
+        if self.sell_in >= 0 and self.quality < 50:
+            self.quality = self.quality + 1
+        elif self.sell_in < 0 and self.quality <= 48:
+            self.quality = self.quality + 2
