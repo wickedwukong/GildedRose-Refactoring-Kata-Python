10c81ef648191e8e25c6322cc10a63cb3257ddd4
Xuemin Guan <xueminguan@Xuemins-MacBook-Pro.local>
2025-10-21 14:58:50 +0100

    XG: implementing the update logic for Conjured item in its own class.

--- MyItem.py
+++ MyItem.py
@@ -2,9 +2,6 @@ from gilded_rose import Item
 
 
 class MyItem(Item):
-    def __init__(self, name, sell_in, quality):
-        super().__init__(name, sell_in, quality)
-
     def update_quality(self):
         if self.name != "Aged Brie" and self.name != "Backstage passes to a TAFKAL80ETC concert":
             if self.quality > 0:
@@ -33,3 +30,21 @@ class MyItem(Item):
             else:
                 if self.quality < 50:
                     self.quality = self.quality + 1
+
+    @staticmethod
+    def make_item(name: str, sell_in: int, quality: int):
+        if name.startswith("Conjured"):
+            return ConjuredItem(name, sell_in, quality)
+        else:
+            return MyItem(name, sell_in, quality)
+
+# Conjured items degrade in quality twice as fast
+class ConjuredItem(MyItem):
+   def update_quality(self):
+       self.sell_in = self.sell_in - 1
+
+       if self.sell_in < 0 and self.quality >= 2:
+           self.quality = self.quality - 2
+       elif self.quality >= 1:
+           self.quality = self.quality - 1
+
--- texttest_fixture.py
+++ texttest_fixture.py
@@ -8,15 +8,15 @@ from gilded_rose import *
 def main():
     print("OMGHAI!")
     items = [
-        MyItem(name="+5 Dexterity Vest", sell_in=10, quality=20),
-        MyItem(name="Aged Brie", sell_in=2, quality=0),
-        MyItem(name="Elixir of the Mongoose", sell_in=5, quality=7),
-        MyItem(name="Sulfuras, Hand of Ragnaros", sell_in=0, quality=80),
-        MyItem(name="Sulfuras, Hand of Ragnaros", sell_in=-1, quality=80),
-        MyItem(name="Backstage passes to a TAFKAL80ETC concert", sell_in=15, quality=20),
-        MyItem(name="Backstage passes to a TAFKAL80ETC concert", sell_in=10, quality=49),
-        MyItem(name="Backstage passes to a TAFKAL80ETC concert", sell_in=5, quality=49),
-        MyItem(name="Conjured Mana Cake", sell_in=3, quality=6),  # <-- :O
+        MyItem.make_item(name="+5 Dexterity Vest", sell_in=10, quality=20),
+        MyItem.make_item(name="Aged Brie", sell_in=2, quality=0),
+        MyItem.make_item(name="Elixir of the Mongoose", sell_in=5, quality=7),
+        MyItem.make_item(name="Sulfuras, Hand of Ragnaros", sell_in=0, quality=80),
+        MyItem.make_item(name="Sulfuras, Hand of Ragnaros", sell_in=-1, quality=80),
+        MyItem.make_item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=15, quality=20),
+        MyItem.make_item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=10, quality=49),
+        MyItem.make_item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=5, quality=49),
+        MyItem.make_item(name="Conjured Mana Cake", sell_in=3, quality=6),  # <-- :O
     ]
     days = 2
     import sys
