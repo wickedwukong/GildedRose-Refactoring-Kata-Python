2a50d4fbc3a7df20a079cb7f5222966c4dec335c
Xuemin Guan <xueminguan@Xuemins-MacBook-Pro.local>
2025-10-22 11:48:38 +0100

    XG: somehow I lost the SulfurasItem class. Bringing it back also simplify the Regular item update logic

--- GildedRoseItem.py
+++ GildedRoseItem.py
@@ -16,22 +16,24 @@ class GildedRoseItem(Item, ABC):
             return AgedBrieItem(name, sell_in, quality)
         elif name == "Backstage passes to a TAFKAL80ETC concert":
             return BackstagePassItem(name, sell_in, quality)
+        elif name == "Sulfuras, Hand of Ragnaros":
+            return SulfurasItem(name, sell_in, quality)
         else:
             return RegularItem(name, sell_in, quality)
 
+class SulfurasItem(GildedRoseItem):
+    def update_quality(self):
+        pass  # Sulfuras does not change in quality or sell_in
 
 class RegularItem(GildedRoseItem):
     def update_quality(self):
         if self.quality > 0:
-            if self.name != "Sulfuras, Hand of Ragnaros":
-                self.quality = self.quality - 1
+            self.quality = self.quality - 1
 
-        if self.name != "Sulfuras, Hand of Ragnaros":
-            self.sell_in = self.sell_in - 1
-        if self.sell_in < 0:
-            if self.quality > 0:
-                if self.name != "Sulfuras, Hand of Ragnaros":
-                    self.quality = self.quality - 1
+        self.sell_in = self.sell_in - 1
+
+        if self.sell_in < 0 < self.quality:
+            self.quality = self.quality - 1
 
 # Conjured items degrade in quality twice as fast
 class ConjuredItem(GildedRoseItem):
