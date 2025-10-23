b3a0bb51e64fc1213b5c08de32cc43ac7d162079
Xuemin Guan <xueminguan@Xuemins-MacBook-Pro.local>
2025-10-23 15:05:05 +0100

    XG: added type annotation to the Item class

--- gilded_rose.py
+++ gilded_rose.py
@@ -8,10 +8,10 @@ class GildedRose:
 
 
 class Item:
-    def __init__(self, name, sell_in, quality):
+    def __init__(self, name: str, sell_in: int, quality : int):
         self.name = name
         self.sell_in = sell_in
         self.quality = quality
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return f"{self.name}, {self.sell_in}, {self.quality}"
